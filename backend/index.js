d094f275d2411cc06095edcf03af93d2b87ecb85
Nathan Stoltenberg <nds4@pdx.edu>
2024-04-20 10:46:45 -0700

    add docker stuff

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/api', apiRouter)
+
+app.use('/', express.static(path.join(__dirname, 'dist', 'fitlog-frontend', 'browser')));
 
 const start = async () => {
     try {
